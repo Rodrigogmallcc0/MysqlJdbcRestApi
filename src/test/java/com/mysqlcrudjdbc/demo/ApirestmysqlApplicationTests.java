693d71d071ffdfc1339ff258cbe0915da8d41b2c
rodrigogutierre <victorgutierrez@upeu.edu.pe>
2023-07-16 23:44:53 -0500

    first commit

---
+++ src/test/java/com/mysqlcrudjdbc/demo/ApirestmysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysqlcrudjdbc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestmysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
