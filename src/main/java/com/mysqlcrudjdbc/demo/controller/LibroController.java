7b780480870031cb927467dff995f904f85dec49
rodrigogutierre <victorgutierrez@upeu.edu.pe>
2023-07-19 15:47:08 -0500

    culminacion de save,update,delete

--- src/main/java/com/mysqlcrudjdbc/demo/controller/LibroController.java
+++ src/main/java/com/mysqlcrudjdbc/demo/controller/LibroController.java
@@ -10,9 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mysqlcrudjdbc.demo.model.Libro;
+import com.mysqlcrudjdbc.demo.model.ServicesResponse;
 import com.mysqlcrudjdbc.demo.service.ILibroServices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -37,5 +43,50 @@ public class LibroController {
     //public SomeData getMethodName(@RequestParam String param) {
     //    return new SomeData();
     //}
+    @GetMapping("/fizzbuzz")
+    public String fizzbuzz(){
+        StringBuilder builder= new StringBuilder();
+        for(int number=1;number<=100;number++){
+        if (number%3==0 & number%5==0) {
+            builder.append( "fizzbuzz");            
+        }if (number%3==0) {
+            builder.append("fizz");
+            
+        } if (number%5==0) {
+            builder.append("buzz");
+        }
+         else{
+            builder.append(number).append(" ");
+        }
+    }   return builder.toString();
+    } 
+    @PostMapping("/save")
+    public ResponseEntity<ServicesResponse> save(@RequestBody Libro libro){
+        ServicesResponse servicesResponse = new ServicesResponse();
+        int result = iLibroServices.save(libro);
+        if(result==1){
+            servicesResponse.setMessage("Libro registrado correctamente");
+        }return new ResponseEntity<>(servicesResponse, HttpStatus.OK);        
+    }
+    @PutMapping("/update")
+    public ResponseEntity<ServicesResponse> update(@RequestBody Libro libro){
+        ServicesResponse servicesResponse = new ServicesResponse();
+        int result = iLibroServices.udpate(libro);
+        if (result==1){
+            servicesResponse.setMessage("Actualizado correctamente");
+        }return new ResponseEntity<>(servicesResponse, HttpStatus.OK);
+    }
+    @PatchMapping("/delete/{id}")
+    public ResponseEntity<ServicesResponse> delete(@PathVariable int id){
+        ServicesResponse servicesResponse = new ServicesResponse();
+        int result = iLibroServices.deleteById(id);
+        if (result==1){
+            servicesResponse.setMessage("Libro eliminado");
+        }
+        return new ResponseEntity<>(servicesResponse, HttpStatus.OK);
+    }
+
+    
+
     
 }
---
+++ src/main/java/com/mysqlcrudjdbc/demo/model/ServicesResponse.java
@@ -0,0 +1,9 @@
+package com.mysqlcrudjdbc.demo.model;
+
+import lombok.Data;
+
+@Data
+public class ServicesResponse {
+    Boolean success;
+    String message;    
+}
